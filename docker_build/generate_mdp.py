d94705cd3132ad3bac26c0402d25f39a4319a662
Tyler Reddy <tyler.je.reddy@gmail.com>
2016-05-05 17:02:25 +0100

    generate_mdp() function now accepts arguments specifying key parameters for the output alchembed .mdp file.

--- docker_build/generate_mdp.py
+++ docker_build/generate_mdp.py
@@ -1,7 +1,10 @@
 import collections
 
-def generate_mdp(resolution='CG', output_filename = 'alchembed.mdp'):
-    '''Produce custom alchembed / GROMACS .mdp file based on Phil F's github tutorial: https://github.com/philipwfowler/alchembed-tutorial'''
+def generate_mdp(resolution='CG', output_filename = 'alchembed.mdp', b = 2, steps = 1000, delta_lambda = 0.001, alpha = 0.1, dt = 0.01):
+    '''Produce custom alchembed / GROMACS .mdp file based on Phil F's github tutorial: https://github.com/philipwfowler/alchembed-tutorial
+    b can be either 1 or 2, but should default to 2 based on suggestion from Alchembed paper
+    steps defaults to 1000 
+    delta_lambda defaults to 0.001'''
     if resolution == 'CG': #Coarse Grained system
         mdp_lines =      collections.OrderedDict([('integrator' , 'md'),
                         ('define'               , '-DPOSRES'),
@@ -34,7 +37,7 @@ def generate_mdp(resolution='CG', output_filename = 'alchembed.mdp'):
                         ('ref_t'                , '310 '),
                         ('gen_vel'              , 'yes'),
                         ('gen_temp'             , '310'),
-                        ('gen_seed'             , '111'),
+                        ('gen-seed'             , '-1'),
                         ('free_energy'          , 'yes'),
                         ('init_lambda'          , '0.00'),
                         ('delta_lambda'         , '1e-3'),
@@ -45,9 +48,16 @@ def generate_mdp(resolution='CG', output_filename = 'alchembed.mdp'):
                         ('couple-lambda0'       , 'none'),
                         ('couple-lambda1'       , 'vdw')])
 
-        with open(output_filename, 'w') as output_file:
-            for key, value in mdp_lines.iteritems():
-                output_file.write(key + ' = ' + value + ',\n')
+    #modify mdp values based on user input, if applicable:
+    mdp_lines['sc-power'] = str(b) # 2 by default, but can change to 1
+    mdp_lines['nsteps'] = str(steps) # 1000 by default, can switch to 10,000 if crashing
+    mdp_lines['delta_lambda'] = str(delta_lambda) #usually 1/steps so that lambda = 1 is reached, but sometimes may want to stop before lambda = 1 if system is really tricky
+    mdp_lines['sc-alpha'] = str(alpha) 
+    mdp_lines['dt'] = str(dt) 
+
+    with open(output_filename, 'w') as output_file:
+        for key, value in mdp_lines.iteritems():
+            output_file.write(key + ' = ' + value + ',\n')
 
 if __name__ == '__main__':
     generate_mdp()
