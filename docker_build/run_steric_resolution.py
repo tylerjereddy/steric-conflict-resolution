94fb744676eb62c338586a17601b820413c40ce2
Tyler Reddy <tyler.je.reddy@gmail.com>
2016-05-07 20:26:10 +0100

    run_steric_resolution.py should now accept a list of start / end indices on the command line.

--- docker_build/run_steric_resolution.py
+++ docker_build/run_steric_resolution.py
@@ -4,10 +4,12 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-input_coord_file_path", type=str)
+parser.add_argument('-index_list', nargs='+', required=True, type=int) #should be list of alternating start and end indices
 args = parser.parse_args()
 
-def run_steric_resolution_loop(input_coord_file = args.input_coord_file_path):
+def run_steric_resolution_loop(input_coord_file = args.input_coord_file_path, index_list = args.index_list):
     print 'input_coord_file:', input_coord_file
+    print 'index_list:', index_list
     
 if __name__ == '__main__':
     run_steric_resolution_loop()
