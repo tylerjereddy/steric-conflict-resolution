badda577476407450510c09f7011fc3df93c8f11
Tyler Reddy <tyler.je.reddy@gmail.com>
2016-05-07 19:51:11 +0100

    Fixed a typo in run_steric_resolution.py.

--- docker_build/run_steric_resolution.py
+++ docker_build/run_steric_resolution.py
@@ -9,6 +9,6 @@ args = parser.parse_args()
 def run_steric_resolution_loop(input_coord_file = args.input_coord_file_path):
     print 'input_coord_file:', input_coord_file
     
-if __name__ = '__main__':
+if __name__ == '__main__':
     run_steric_resolution_loop()
 
