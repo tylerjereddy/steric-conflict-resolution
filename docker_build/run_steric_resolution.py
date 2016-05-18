6cd8be2d66bd7863467a649398c8f30ae56ddabc
Tyler Reddy <tyler.je.reddy@gmail.com>
2016-05-18 14:34:04 +0100

    Increased the verbosity of the mdrun command in run_steric_resolution.py to facilitate debug efforts.

--- docker_build/run_steric_resolution.py
+++ docker_build/run_steric_resolution.py
@@ -99,7 +99,7 @@ def run_steric_resolution_loop(input_coord_file = args.input_coord_file_path, in
 
         #run the alchembed 'simulation' on a single core
         print 'starting alchembed simulation for round ', round_number
-        subprocess.call(['/bin/bash','-i','-c','gmx mdrun -s {tpr_filename} -deffnm alchembed_round_{round_number} -nt 1'.format(tpr_filename=tpr_filename, round_number=round_number)])
+        subprocess.call(['/bin/bash','-i','-c','gmx mdrun -v -stepout 100 -s {tpr_filename} -deffnm alchembed_round_{round_number} -nt 1'.format(tpr_filename=tpr_filename, round_number=round_number)])
         print 'finished alchembed simulation for round ', round_number
         round_number += 1
 
