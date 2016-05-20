599db18072beba7b7ae7f34fbaf5b69ca91f3910
Tyler Reddy <tyler.je.reddy@gmail.com>
2016-05-20 14:04:42 +0100

    Fixed DPPC argument in test_resolution_steric_conflicts() unit test.

--- docker_build/tests.py
+++ docker_build/tests.py
@@ -10,7 +10,7 @@ class TestSimpleCopiesDPPC(unittest.TestCase):
     '''Test that the docker image can completely resolve steric conflicts in the simple case of two superposed DPPC molecules.'''
 
     def test_resolution_steric_conflicts(self):
-        run_steric_resolution.run_steric_resolution_loop(input_coord_file = '/steric_conflict_resolution_work/test_data/dppc_simple_copies/dppc_simple_copies.gro', index_list = [1, 24], residue_names_list = DPPC, cutoff = 2.0, list_particles_per_residue = 12, output_path = '/steric_conflict_resolution_work/dppc_simple_test', topology_filepath = '/steric_conflict_resolution_work/test_data/dppc_simple_copies/sys.top')
+        run_steric_resolution.run_steric_resolution_loop(input_coord_file = '/steric_conflict_resolution_work/test_data/dppc_simple_copies/dppc_simple_copies.gro', index_list = [1, 24], residue_names_list = 'DPPC', cutoff = 2.0, list_particles_per_residue = 12, output_path = '/steric_conflict_resolution_work/dppc_simple_test', topology_filepath = '/steric_conflict_resolution_work/test_data/dppc_simple_copies/sys.top')
         list_steric_conflicts_by_round = []
         for steric_conflict_pickle_file in glob.glob('/steric_conflict_resolution_work/dppc_simple_test/cumulative_array_per_residue_steric_conflicts_round_*.p'):
             steric_conflict_data_array = pickle.load(open(steric_conflict_pickle_file, 'rb'))
