cefff6a07a9f59365d7d1fdb2176476601c63f31
Tyler Reddy <tyler.je.reddy@gmail.com>
2016-05-22 15:49:53 +0100

    Added a unit test for the multiple MARTINI residue type coordinates.

--- docker_build/tests.py
+++ docker_build/tests.py
@@ -60,5 +60,18 @@ class TestLoopArgCheck(unittest.TestCase):
             run_steric_resolution.run_steric_resolution_loop(input_coord_file='dummy', index_list = [1, 24,48,60], residue_names_list = ['DPPC', 'POPS'], cutoff = 2.0, list_particles_per_residue = [12], output_path = 'dummy', topology_filepath = 'dummy', alchembed_b_value = 2, alchembed_resolution = 'CG', alchembed_steps = 1000, alchembed_alpha = 0.1, alchembed_dt = 0.01)
         self.assertEqual('The list_particles_per_residue should be half as long as the index_list as the latter contains start & end indices for each residue.', context.exception.message)
             
+class TestMultipleResiduesCG(unittest.TestCase):
+    '''Test that the docker image can completely resolve steric conflicts in the case of several superposed MARTINI lipid residues of different types.'''
+
+    def test_resolution_steric_conflicts(self):
+        os.mkdir('/steric_conflict_resolution_work/multi_residue_cg_test') #probably don't need a proper temporary object for now because only running on travis where it will get wiped anyway
+        run_steric_resolution.run_steric_resolution_loop(input_coord_file = '/steric_conflict_resolution_work/test_data/multiple_lipids_MARTINI/multiple_MARTINI_residues_steric_conflicts.gro', index_list = [1, 60, 61, 130, 131, 250, 251, 325], residue_names_list = ['DOPC', 'PRPC', 'XNG3', 'XNSM'], cutoff = 2.0, list_particles_per_residue = [12, 14, 24, 15], output_path = '/steric_conflict_resolution_work/multi_residue_cg_test', topology_filepath = '/steric_conflict_resolution_work/test_data/multiple_lipids_MARTINI/sys.top')
+        list_steric_conflicts_by_round = []
+        for steric_conflict_pickle_file in glob.glob('/steric_conflict_resolution_work/multi_residue_cg_test/cumulative_array_per_residue_steric_conflicts_round_*.p'):
+            steric_conflict_data_array = pickle.load(open(steric_conflict_pickle_file, 'rb'))
+            num_residues_with_steric_conflicts = np.count_nonzero(steric_conflict_data_array)
+            list_steric_conflicts_by_round.append(num_residues_with_steric_conflicts)
+        self.assertEqual(min(list_steric_conflicts_by_round), 0, "The multiple_MARTINI_residues_steric_conflicts.gro file should have all steric conflicts resolved by the last round of steric resolution.")
+
         
 
